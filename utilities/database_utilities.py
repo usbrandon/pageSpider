b1087d4f7a05d7f11e521de7612412609aeb5224
Brandon Jackson <usbrandon@gmail.com>
2021-08-22 11:16:01 -0500

    Initial commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,7 @@
+def create_database(database_path: str):
+    #TODO: Generate the database
+    pass
+
+def save_words_to_database(database_path: str, words_list: list):
+    #TODO: Save the words to the database
+    pass

7934afd01adbe2a35b5fc952b718b6e59a5a5e40
Brandon Jackson <usbrandon@gmail.com>
2021-08-22 11:33:19 -0500

    Fixed formatting issues

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,7 +1,8 @@
 def create_database(database_path: str):
-    #TODO: Generate the database
+    # TODO: Generate the database
     pass
 
+
 def save_words_to_database(database_path: str, words_list: list):
-    #TODO: Save the words to the database
+    # TODO: Save the words to the database
     pass
